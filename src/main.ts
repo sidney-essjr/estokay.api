aa8f73645e3120b5cebd9aaff9d0de8f9183506d
sidney-essjr <sidney@sessjr.com>
2024-11-13 18:23:50 -0300

    config: porta de saida

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
     }),
   );
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 10000;
   await app.listen(port, '0.0.0.0');
 }
 bootstrap();

7466ec35e148ac8a83fcfd71577bf1afa4334844
sidney-essjr <sidney@sessjr.com>
2024-11-13 18:42:45 -0300

    update cors

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://localhost:5173', 'https://estokay-front.vercel.app/'],
+    origin: ['https://localhost:5173', 'https://estokay-front.vercel.app'],
     credentials: true,
   });
 

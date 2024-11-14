bbc9cced5c6ffc499ca08f814e27b5dcd0ee6225
sidney-essjr <sidney@sessjr.com>
2024-11-14 16:50:54 -0300

    config https

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { readFileSync } from 'fs';
 
 async function bootstrap() {
-  // const httpsOptions = {
-  //   key: readFileSync('certificates/key.pem'),
-  //   cert: readFileSync('certificates/cert.pem'),
-  // };
+  const httpsOptions = {
+    key: readFileSync('certificates/key.pem'),
+    cert: readFileSync('certificates/cert.pem'),
+  };
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { httpsOptions });
 
   app.enableCors({
     origin: ['https://localhost:5173', 'https://estokay-front-1.onrender.com'],
